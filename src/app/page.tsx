3c89acff85925f6e37ee451fe8d78a20ac100988
esunn0412 <taeeunk1208@gmail.com>
2024-08-21 13:59:23 +0900

    문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ const Home = () =>{
                 </div>
               </div>
               {[
-                { icon: "/icon1.svg", title: "<span class='text-[#EF8F00]'>무료</span>로", description: "합리적인 비용으로 고품질 맞춤법 교정 제공, 소외 계층, 유학생, 한국어를 공부하는 외국인 등"},
+                { icon: "/icon1.svg", title: "<span class='text-[#EF8F00]'>무료</span>로", description: "무료로 고품질 맞춤법 교정 제공, 소외 계층, 유학생, 한국어를 공부하는 외국인 등"},
                 { icon: "/icon2.svg", title: "<span class='text-[#EF8F00]'>가장 편한</span> 형태로", description: "웹사이트와 크롬 익스텐션으로 이용 가능 실시간 맞춤법 체크 지원 사용자 피드백을 반영하여 서비스 개선"},
                 { icon: "/icon3.svg", title: "<span class='text-[#EF8F00]'>정확한</span> 문장 교정", description: "한국어 맞춤법 규정 반영 최신 자연어 처리 기술 (llama-3-70b) 적용 문맥을 고려한 지능형 교정 지속적 업데이트" },
               ].map((item, index) => (
